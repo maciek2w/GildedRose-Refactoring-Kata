7c51a4504fb26ccb41bc4660d48283a2c7c2411d
Maciej Witaszek <mwitaszek@dminc.com>
2016-10-31 16:30:38 +0700

    Refactored method updateQualityBackstagePasses

--- swift/GildedRose/GildedRose.swift
+++ swift/GildedRose/GildedRose.swift
@@ -100,26 +100,23 @@ public class GildedRose {
   }
   
   func updateQualityBackstagePasses(in item: Item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1
-      
-      if (item.sellIn < 11) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
-      }
-      
-      if (item.sellIn < 6) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
-      }
-    }
-    
     item.sellIn = item.sellIn - 1
     
     if (item.sellIn < 0) {
       item.quality = 0
+    }
+    else {
+      item.quality = item.quality + 1
+      
+      if (item.sellIn < 10) {
+        item.quality = item.quality + 1
+      }
+      
+      if (item.sellIn < 5) {
+          item.quality = item.quality + 1
+      }
+      
+      item.quality = min(item.quality, 50)
     }
   }
 }
