3aec52e27c8f9a398419348e6de0f2edeeb5d3ac
Maciej Witaszek <mwitaszek@dminc.com>
2016-10-31 15:39:40 +0700

    Removed AgedBrie code from big method

--- swift/GildedRose/GildedRose.swift
+++ swift/GildedRose/GildedRose.swift
@@ -24,7 +24,7 @@ public class GildedRose {
       return
     }
     
-    if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
       if (item.quality > 0) {
         if (item.name != "Sulfuras, Hand of Ragnaros") {
           item.quality = item.quality - 1
@@ -55,20 +55,14 @@ public class GildedRose {
     }
     
     if (item.sellIn < 0) {
-      if (item.name != "Aged Brie") {
-        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.quality > 0) {
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-              item.quality = item.quality - 1
-            }
+      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.quality > 0) {
+          if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.quality = item.quality - 1
           }
-        } else {
-          item.quality = item.quality - item.quality
         }
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
+        item.quality = item.quality - item.quality
       }
     }
   }
