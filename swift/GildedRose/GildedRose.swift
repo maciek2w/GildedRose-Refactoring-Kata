a853880865d8c15efc0e2c404beed64c90799938
Maciej Witaszek <mwitaszek@dminc.com>
2016-10-31 15:44:46 +0700

    Extracted updateQualitySulfurasHandofRagnaros(in item: Item)

--- swift/GildedRose/GildedRose.swift
+++ swift/GildedRose/GildedRose.swift
@@ -24,11 +24,14 @@ public class GildedRose {
       return
     }
     
+    if (item.name == "Sulfuras, Hand of Ragnaros") {
+      updateQualitySulfurasHandofRagnaros(in: item)
+      return
+    }
+    
     if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
       if (item.quality > 0) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-          item.quality = item.quality - 1
-        }
+        item.quality = item.quality - 1
       }
     } else {
       if (item.quality < 50) {
@@ -50,16 +53,12 @@ public class GildedRose {
       }
     }
     
-    if (item.name != "Sulfuras, Hand of Ragnaros") {
-      item.sellIn = item.sellIn - 1
-    }
+    item.sellIn = item.sellIn - 1
     
     if (item.sellIn < 0) {
       if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
         if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1
-          }
+          item.quality = item.quality - 1
         }
       } else {
         item.quality = item.quality - item.quality
@@ -89,5 +88,9 @@ public class GildedRose {
       
       item.quality = item.quality + 1
     }
+  }
+  
+  func updateQualitySulfurasHandofRagnaros(in item: Item) {
+    //Do nothing
   }
 }
